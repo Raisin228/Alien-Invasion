691bad3e6d9c28f95e07694fddc4c9b13ffea1a3
Bogdan Atrosenko <bogdanatrosenko@gmail.com>
2023-02-03 17:23:47 +0300

    На экране появился дреднаут

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,11 @@
 import sys
 import pygame
+from settings import Settings
+from my_ship import Ship
+
+FPS = 60
+clock = pygame.time.Clock()
+
 
 class AlienInvansion():
     '''Класс для управления ресурсами и поведением игры.'''
@@ -7,8 +13,10 @@ class AlienInvansion():
     def __init__(self):
         '''Инициализируем игру и создаём игровые ресурсы'''
         pygame.init()
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption('Alien Invansion')
+        self.ship = Ship(self)
 
     def run_game(self):
         '''Запуск основного цикла игры.'''
@@ -18,12 +26,15 @@ class AlienInvansion():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            # устaнавливаем цвет фона
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+
             # отображение последнего прорисованного экрана
             pygame.display.flip()
+            clock.tick(FPS)
+
 
 if __name__ == '__main__':
     ai = AlienInvansion()
     ai.run_game()
-
-
-
